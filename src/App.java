084eaddd6d3795425d89786dec25bb86bdabc75b
Caio Henrique <caio.henrique.20111@gmail.com>
2022-05-13 14:24:08 -0300

    Inicio do projeto.

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // ETAPA 2
+        // • Com a tabela da etapa 1, crie um método para carregar palavras de um arquivo fornecido por parâmetro. Este arquivo pode 
+        // conter mais de 1 milhão de palavras, então esteja preparado para isso. 
+        // • A partir do arquivo fornecido, a tabela deve armazenar somente as palavras que contenham exatamente 5 letras.
+        
+    }
+}
---
+++ src/TabelaHash.java
@@ -0,0 +1,31 @@
+public class TabelaHash {
+
+    // ETAPA 1
+    // • Crie uma tabela hash com entradas armazenando palavras. A chave da palavra é ela mesma, ou seja, o par da entrada terá 
+    // os tipos <String, String> e o valor dos dois atributos será a mesma palavra.
+    // • Teste o funcionamento da sua tabela para as operações de inserção e busca com o código base e o arquivo de 10 palavras 
+    // fornecido pelos professores.
+    // • Crie duas operações na sua tabela, com as assinaturas e atribuições especificadas a seguir:
+
+    /**
+    * Este método recebe uma posição inteira com parâmetro e retorna 
+    * a palavra nesta posição na tabela. Se a posição não contiver
+    * uma palavra, deve retornar a palavra da posição mais próxima.
+    * @param posicao Número inteiro representando a posição da tabela.
+    * @return Uma palavra constante da tabela
+    */
+    public String sortearPalavra(int posicao){
+        return "";
+    }
+
+    /**
+    * Este método recebe uma string contendo uma palavra. Ele deve 
+    * buscar esta palavra na tabela, retornando TRUE se a palavra
+    * existe ou FALSE se ela não existir
+    * @param palavra A palavra a ser procurada na tabela
+    * @return TRUE/FALSE conforme a existência da palavra.
+    */
+    public boolean existePalavra(String palavra){
+        return true;
+    }
+}
